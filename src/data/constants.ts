95c3ef43cdc5c6f8eb0fd1e2ff7c82b35e2b143e
TTL <thanh.187pm14010@vanlanguni.vn>
2023-08-19 14:37:25 +0700

    fix social links and some greetings

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,9 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	github: "https://github.com/sonny2k",
+	linkedin: "https://www.linkedin.com/in/sonny2k/",
+	twitter: "https://twitter.com/0xNGOC",
+	discord: "https://discordapp.com/users/847314404794040341",
+	email: "thanhluu.business@gmail.com",
 };
